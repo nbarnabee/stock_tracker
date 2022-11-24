328710d97b3d30b225a381f8a5e664c025b9b9bf
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-11-24 15:00:52 +0100

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,22 @@
+const MongoClient = require("mongodb").MongoClient;
+const assert = require("assert");
+
+let connection = [];
+
+establishConnection = function (callback) {
+  MongoClient.connect(process.env.DB_STRING, function (err, db) {
+    assert.equal(null, err);
+    connection = db;
+    console.log(`Connection established`);
+    if (typeof callback === "function" && callback) callback(connection);
+  });
+};
+
+function getConnection() {
+  return connection;
+}
+
+module.exports = {
+  establishConnection: establishConnection,
+  getConnection: getConnection,
+};
